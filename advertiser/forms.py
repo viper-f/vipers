b201b5d471bbc3dea5fe57563df49bbe151e8ee7
viper-f <viper.frpg@outlook.com>
2023-09-26 08:44:53 -0400

    style

--- advertiser/forms.py
+++ advertiser/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 
 class UrlForm(forms.Form):
-    url = forms.CharField(label="Url", max_length=100)
+    url = forms.CharField(label="Url", max_length=100, widget=forms.TextInput(attrs={'class': 'sul-text-field'}))
