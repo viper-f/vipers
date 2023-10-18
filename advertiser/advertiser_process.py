da524d1e2bd282b504f196e44db0c45c2eae02c1
Kate Makarova <kate.makarova@hotmail.com>
2023-10-18 08:45:47 -0400

    advertiser process

--- advertiser/advertiser_process.py
+++ advertiser/advertiser_process.py
@@ -13,13 +13,54 @@ import vipers
 
 parser = OptionParser()
 parser.add_option("-u", '--url', dest="base_url")
+parser.add_option("-su", '--start-url', dest="start_url")
+parser.add_option("-t", '--template', dest="template")
+parser.add_option("-cc", '--custom-credentials', dest="custom_credentials")
+parser.add_option("-cu", '--custom-username', dest="custom_username")
+parser.add_option("-cp", '--custom-password', dest="custom_password")
 (options, args) = parser.parse_args()
 
 channel_layer = get_channel_layer()
 group_name = 'test'
 
-# advertiser = Advertiser(log_mode='channel', channel=channel_layer, group_name=group_name)
-# advertiser.work(options.base_url)
+stop_list = [
+    "https://vampdynasties.mybb.ru",
+    "https://curama.mybb.ru",
+    "http://curama.mybb.ru",
+    "http://themostsupernatural.ru",
+    "https://themostsupernatural.ru"
+    # "https://faceinless.ru",
+    # "https://crossteller.ru",
+    # "https://incident.rusff.me",
+    # "http://fear.rusff.me/viewtopic.php?id=54&p=9
+]
+
+custom_login_code = {
+    "https://phoenixlament.f-rpg.me": "PiarIn()",
+    "https://execute.rusff.me": "PR['in_2']()",
+    "https://intovoid.f-rpg.me": "PR['in_2']()",
+    "https://sacramento.rusff.me": "PrLogin('pr')",
+    "https://asphodel.rusff.me": "PR['in_2']()",
+    "https://timess.rusff.me": "PR['in_2']()",
+    "https://verbaveritatis.rusff.me": "PR['in_2']()",
+    "https://miyron.rolka.me": "PiarIn(PiarNik1,PiarPas1)",
+    "https://drinkbutterbeer.ru": "PrLogin('pr')",
+    "https://dragonageone.mybb.ru": "PR['in_2']()",
+    "https://toeden.rusff.me": "PR['in_2']()",
+    "https://kakbicross.ru": "PR['in_2']()",
+    "https://daas.rusff.me": "PR['in_2']()"
+}
+
+advertiser = Advertiser(log_mode='channel', channel=channel_layer, group_name=group_name, data_grab=False)
+if options.custom_credentians:
+    advertiser.custom_login(url=options.base_url, username=options.custom_username, password=options.custom_password)
+advertiser.work(
+    url=options.base_url,
+    start_url=options.start_url,
+    stop_list=stop_list,
+    template=options.template,
+    custom_login_code=custom_login_code
+)
 
 for i in range(20):
     async_to_sync(channel_layer.group_send)(
