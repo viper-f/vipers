b2906bd64c52b2f81b2d6bdc15e489f1d0c73a2c
Kate Makarova <kate.makarova@hotmail.com>
2023-10-19 08:19:53 -0400

    --

--- advertiser/forms.py
+++ advertiser/forms.py
@@ -4,7 +4,7 @@ from django import forms
 class UrlForm(forms.Form):
     url = forms.CharField(label="Url", max_length=100, widget=forms.TextInput(attrs={'class': 'sul-text-field'}))
     start_url = forms.CharField(label="Start Url", max_length=100, widget=forms.TextInput(attrs={'class': 'sul-text-field'}))
-    template = forms.CharField(label="template", widget=forms.Textarea(attrs={"rows": "5", 'class': 'sul-text-field'}))
+    template = forms.CharField(label="Template", widget=forms.Textarea(attrs={"rows": "5", 'class': 'sul-text-field'}))
     custom_credentials = forms.BooleanField(label="Custom Credentials", widget=forms.CheckboxInput(attrs={'class': 'sul-checkbox-type-2'}))
     custom_username = forms.CharField(label="Custom Username", max_length=100, widget=forms.TextInput(attrs={'class': 'sul-text-field'}))
     custom_password = forms.CharField(label="Custom Password", max_length=100, widget=forms.TextInput(attrs={'class': 'sul-text-field'}))
