964fe01e238f085dc0c07b8f1729a31f17a4420c
Kate Makarova <kate.makarova@hotmail.com>
2023-10-22 19:41:45 -0400

    --

--- vipers/forms.py
+++ vipers/forms.py
@@ -3,4 +3,4 @@ from django import forms
 
 class LoginForm(forms.Form):
     username = forms.CharField(label="Username", max_length=100, widget=forms.TextInput(attrs={'class': 'sul-text-field'}))
-    password = forms.CharField(label="Password", max_length=100, widget=forms.TextInput(attrs={'class': 'sul-text-field'}))
+    password = forms.CharField(label="Password", max_length=100, widget=forms.PasswordInput(attrs={'class': 'sul-text-field'}))
