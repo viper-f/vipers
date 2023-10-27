854181011b218dc6395399841785d5b1756d9e36
viper-f <143018173+viper-f@users.noreply.github.com>
2023-10-27 14:32:23 -0400

    Update admin.py

--- advertiser/admin.py
+++ advertiser/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from advertiser.models import Forum, HomeForum
+from advertiser.models import Forum, HomeForum, BotSession
 
 admin.site.register(Forum)
 admin.site.register(HomeForum)
