7e28661dd4ee1e898999cf3226b76ab6037899c3
Kate Makarova <kate.makarova@hotmail.com>
2023-11-10 20:23:41 -0500

    --

--- advertiser/cron.py
+++ advertiser/cron.py
@@ -4,7 +4,10 @@ import subprocess
 import random
 import sys
 
-from advertiser.models import ScheduleItem, BotSession, HomeForum, AdTemplate
+from django.contrib.auth.models import User
+from django.db import connection
+from advertiser.AdvertiserV2 import AdvertiserV2
+from advertiser.models import ScheduleItem, BotSession, HomeForum, AdTemplate, Forum
 from django.utils import timezone
 
 def scheduled_bot_run():
@@ -50,20 +53,64 @@ def scheduled_bot_run():
 
     autorun_id = 7
     session_id = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
-    url = forum.ad_topic_url
     templates = list(AdTemplate.objects.filter(home_forum=forum.id).order_by("priority").values_list('id', flat=True))
-    templates = [str(i) for i in templates]
-    print('schedule 3')
 
-    subprocess.Popen(["venv/bin/python", "advertiser/advertiser_process.py",
-                      "-l", url,
-                      "-i", session_id,
-                      "-t", ','.join(templates),
-                      "-c", custom_credentials,
-                      "-u", custom_username,
-                      "-p", custom_password,
-                      '-f', str(forum.id),
-                      '-q', str(autorun_id),
-                      "symbol"], stdout=open('subprocess.log', 'a'), stderr=open('subprocess.errlog', 'a'))
+    user = User.objects.get(pk=autorun_id)
+    now = timezone.now()
+    record = BotSession(
+        type='advertiser',
+        home_forum=forum,
+        user=user,
+        session_id=session_id,
+        status='active',
+        time_start=now.isoformat()
+    )
+    try:
+        record.save()
+    except:
+        print('This session was already started')
+        return False
+
+
+    cl_forums = Forum.objects.filter(custom_login__isnull=False)
+    custom_login_code = {}
+    for cl_forum in cl_forums:
+        custom_login_code[cl_forum.domain] = cl_forum.custom_login
+
+    stop_list = list(Forum.objects.filter(stop=True).values_list('domain', flat=True))
+
+    advertiser = AdvertiserV2(log_mode='console',  session_id=session_id)
+
+
+    if custom_credentials == 'true':
+        advertiser.custom_login(url=forum.ad_topic_url, username=custom_username,  password=custom_password)
+
+    visited, success, links = advertiser.work(
+        url=forum.ad_topic_url,
+        home_forum_id=forum.forum.id,
+        templates=templates,
+        custom_login_code=custom_login_code,
+        stop_list=stop_list
+    )
+
+    sql_links = []
+    if len(links):
+        for link in links:
+            if link[2] == 'new' and link[1] != 0:
+                sql_links.append("('" + link[0] + "'," + str(link[1]) + ")")
+        sql_links = ', '.join(sql_links)
+        if len(sql_links):
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    "INSERT INTO advertiser_forum (domain, verified_forum_id) VALUES " + sql_links + " ON CONFLICT DO NOTHING")
+
+    now = timezone.now()
+    record.time_end = now.isoformat()
+    record.visited = visited
+    record.success = success
+    record.status = 'finished'
+    record.save()
+
+
 
 
