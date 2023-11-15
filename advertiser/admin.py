9f63e54406c8675c2c49159c6dffd1d43284b11b
Kate Makarova <kate.makarova@hotmail.com>
2023-11-14 20:26:14 -0500

    --

--- advertiser/admin.py
+++ advertiser/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from advertiser.models import Forum, HomeForum, BotSession, ScheduleItem, WantedUpdate
+from advertiser.models import Forum, HomeForum, BotSession, ScheduleItem, WantedUpdate, CustomCredentials
 
 admin.site.register(Forum)
 admin.site.register(HomeForum)
 admin.site.register(BotSession)
 admin.site.register(ScheduleItem)
 admin.site.register(WantedUpdate)
+admin.site.register(CustomCredentials)
