0ce714ad8d6703cfe6ce6c01a74186d8214b5c62
Kate Makarova <kate.makarova@hotmail.com>
2023-12-09 18:05:03 -0500

    remove export

--- advertiser/admin.py
+++ advertiser/admin.py
@@ -1,15 +1,8 @@
 from django.contrib import admin
-from import_export.admin import ImportExportModelAdmin
-from .resource import ForumResource
-
 from advertiser.models import Forum, HomeForum, BotSession, ScheduleItem, WantedUpdate, CustomCredentials
 
 
-class ForumAdmin(ImportExportModelAdmin):
-    resource_class = ForumResource
-
-
-admin.site.register(Forum, ForumAdmin)
+admin.site.register(Forum)
 admin.site.register(HomeForum)
 admin.site.register(BotSession)
 admin.site.register(ScheduleItem)
