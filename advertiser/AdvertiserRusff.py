babf5dd63aa7d49fb13873162eb59c52592b9059
Kate Makarova <kate.makarova@hotmail.com>
2023-12-09 18:22:56 -0500

    multiple sessions

--- advertiser/AdvertiserRusff.py
+++ advertiser/AdvertiserRusff.py
@@ -26,6 +26,7 @@ class AdvertiserRusff:
     def __init__(self, log_mode='console', channel=None, session_id=None):
         options = Options()
         options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
         options.add_argument('--disable-gpu')
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
         prefs = {"profile.managed_default_content_settings.images": 2}
