16046ba7b15ac46019947659cee8d160530451f4
Kate Makarova <kate.makarova@hotmail.com>
2023-12-11 18:40:18 -0500

    bugfix

--- advertiser/PartnerMailer.py
+++ advertiser/PartnerMailer.py
@@ -17,6 +17,7 @@ class PartnerMailer:
 
         options = Options()
         options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
         options.add_argument('--disable-gpu')
 
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
