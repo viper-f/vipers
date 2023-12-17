51607bd81a2d13f8f22cdf64ebbd61cc72e30e62
Kate Makarova <kate.makarova@hotmail.com>
2023-12-17 13:23:47 -0500

    fix

--- advertiser/admin.py
+++ advertiser/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from advertiser.models import Forum, HomeForum, BotSession, ScheduleItem, WantedUpdate, CustomCredentials
-from admin_searchable_dropdown.filters import AutocompleteFilter
-
 
 
 admin.site.register(BotSession)
