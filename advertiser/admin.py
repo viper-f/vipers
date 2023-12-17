533725de805f2f6f7e500491a43621344727a746
Kate Makarova <kate.makarova@hotmail.com>
2023-12-17 13:13:34 -0500

    autocomplete

--- advertiser/admin.py
+++ advertiser/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
 from advertiser.models import Forum, HomeForum, BotSession, ScheduleItem, WantedUpdate, CustomCredentials
+from admin_searchable_dropdown.filters import AutocompleteFilter
 
 
-admin.site.register(Forum)
-admin.site.register(HomeForum)
+
 admin.site.register(BotSession)
 admin.site.register(ScheduleItem)
 admin.site.register(WantedUpdate)
 admin.site.register(CustomCredentials)
+
+
+class ForumAdmin(admin.ModelAdmin):
+    search_fields = ['domain']
+
+
+class HomeForumAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['forum']
+
+
+admin.site.register(Forum, ForumAdmin)
+admin.site.register(HomeForum, HomeForumAdmin)
