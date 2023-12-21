377ae8c3294fe946edd019c3eaf2814e618d895a
Kate Makarova <kate.makarova@hotmail.com>
2023-12-20 23:35:23 -0500

    check

--- advertiser/ActivityChecker.py
+++ advertiser/ActivityChecker.py
@@ -10,7 +10,7 @@ from requests.exceptions import SSLError
 sys.path.insert(0, './../vipers')
 import vipers
 import django
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vipers.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vipers.settings")
 django.setup()
 from django.db import connection
 from advertiser.models import Forum
@@ -20,7 +20,7 @@ class ActivityChecker:
         self.forums = self.load_forums()
 
     def load_forums(self):
-        return Forum.objects.filter(stop=False)
+        return list(Forum.objects.exclude(stop=True).values_list('id', 'domain', 'inactive_days', 'board_found'))
 
     def check_activity_24(self, url):
         try:
@@ -40,43 +40,32 @@ class ActivityChecker:
         now = time.time()
         if now - founded > 2628000:  # 1 month
             if inactive_days >= 5:
-                return True
+                return 'true'
             else:
-                return False
+                return 'false'
         else:
             if inactive_days >= 15:
-                return True
+                return 'true'
             else:
-                return False
+                return 'false'
 
     def work(self):
         values = []
         for forum in self.forums:
-            number = self.check_activity_24(forum.domain)
+            number = self.check_activity_24(forum[1])
             if number < 5:
                 days = forum[2] + 1
             else:
                 days = 0
-            is_dead = self.is_forum_dead(days, forum.board_found)
-            forum.stop = is_dead
-            forum.activity = number
-            forum.inactive_days = days
-            forum.save()
-        #     values.append('(' + str(number) + ',' + str(days) + ',' + is_dead + ',' + str(forum[0]) + ')')
-        # values = ','.join(values)
-        # #print("update advertiser_forum as forum set activity = c.activity, inactive_days = c.days, stop = c.is_dead from (values " + values + ") as c(activity, days, is_dead, id) where c.id = forum.id;")
-        #
-        # connection.autocommit = True
-        # with connection.cursor() as cursor:
-        #     results = cursor.execute(
-        #         "update advertiser_forum as forum set activity = c.activity, inactive_days = c.days, stop = c.is_dead from (values " + values + ") as c(activity, days, is_dead, id) where c.id = forum.id;")
-        #     connection.commit()
-        #
-        #     try:
-        #         for result in results:
-        #             pass
-        #     except Exception as e:
-        #         pass
-        #
-        #     cursor.close()
-        #    connection.close()
+            is_dead = self.is_forum_dead(days, forum[3])
+            values.append('(' + str(number) + ',' + str(days) + ',' + is_dead + ',' + str(forum[0]) + ')')
+        values = ','.join(values)
+        #print("update advertiser_forum as forum set activity = c.activity, inactive_days = c.days, stop = c.is_dead from (values " + values + ") as c(activity, days, is_dead, id) where c.id = forum.id;")
+
+        with connection.cursor() as cursor:
+            results = cursor.execute(
+                "update advertiser_forum as forum set activity = c.activity, inactive_days = c.days, stop = c.is_dead from (values " + values + ") as c(activity, days, is_dead, id) where c.id = forum.id;")
+            connection.commit()
+
+            cursor.close()
+            connection.close()
