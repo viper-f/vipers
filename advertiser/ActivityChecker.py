2d81166d8419a30999158600c354d8d33c48eabf
Kate Makarova <kate.makarova@hotmail.com>
2024-01-18 19:43:39 -0500

    activity log

--- advertiser/ActivityChecker.py
+++ advertiser/ActivityChecker.py
@@ -69,7 +69,7 @@ class ActivityChecker:
             cursor.execute(
                 "update advertiser_forum as forum set activity = c.activity, inactive_days = c.days from (values " + values + ") as c(activity, days, id) where c.id = forum.id;")
             cursor.execute(
-                "INSERT INTO advertiser_activityrecord (forum, activity, day) SELECT id, activity, CURRENT_DATE FROM advertiser_forum WHERE stop <> TRUE;")
+                "INSERT INTO advertiser_activityrecord (forum_id, activity, day) SELECT id, activity, CURRENT_DATE FROM advertiser_forum WHERE stop <> TRUE;")
             cursor.execute(
                 "update advertiser_forum as forum set stop = c.is_dead from (values " + values + ") as c(is_dead, id) where c.id = forum.id;")
 
