eae1c03d32d9e0cad130f3120f5f6c9f1c5eec2f
viper-f <143018173+viper-f@users.noreply.github.com>
2024-03-24 21:05:21 -0400

    Trainer.py for model training

--- intellect/Trainer.py
+++ intellect/Trainer.py
@@ -1,6 +1,4 @@
-import shutil
-
-from django.utils import timezone
+# from django.utils import timezone
 import os
 from random import shuffle
 import pickle
@@ -10,7 +8,10 @@ import tensorflow as tf
 import keras
 from bs4 import BeautifulSoup
 from intellect.Intellect import Intellect
-from intellect.models import Page, TrainingSet, CrawlSession
+from sklearn.model_selection import train_test_split
+
+
+# from intellect.models import Page, TrainingSet, CrawlSession
 
 
 class Trainer:
@@ -25,7 +26,7 @@ class Trainer:
         labels = []
         for page in pages:
             if self.root_path:
-                path = page.file_path.replace('./pages', self.root_path+'/pages')
+                path = page.file_path.replace('./pages', self.root_path + '/pages')
             else:
                 path = page.file_path
             f = open(path, "r", encoding="windows-1251")
@@ -73,13 +74,6 @@ class Trainer:
             shuffle_labels.append(shuffled_label)
         return shuffle_data, shuffle_labels
 
-    def remake_training_set(self, crawl_session_id, shuffle_number=3):
-        session = CrawlSession.objects.get(pk=crawl_session_id)
-        folder_path = './../training_sets/' + session.session_id
-        shutil.rmtree(folder_path)
-        TrainingSet.objects.filter(control_session_id=crawl_session_id).delete()
-        self.make_training_set(crawl_session_id, shuffle_number)
-
     def make_training_set(self, crawl_session_id, shuffle_number=3):
         session = CrawlSession.objects.get(pk=crawl_session_id)
         dataset, labels = self.form_dataset(crawl_session_id)
@@ -118,34 +112,63 @@ class Trainer:
         set.save()
 
     def train(self, folder_name):
-        folder_path = './../training_sets/'+folder_name
+        folder_path = './../training_sets/' + folder_name
         with open(folder_path + '/dataset.pickle', 'rb') as output:
             dataset = pickle.load(output)
         with open(folder_path + '/labels.pickle', 'rb') as output:
             labels = pickle.load(output)
 
-        index = round(len(dataset) * 0.8)
-        training_dataset, test_dataset = dataset[:index], dataset[index:]
-        training_labels, test_labels = labels[:index], labels[index:]
+        for i in range(0, 3):
+            shuffle_data, shuffle_labels = self.shuffle_dataset(dataset, labels)
+            dataset += shuffle_data
+            labels += shuffle_labels
+
+        dataset = np.array(dataset)
+        labels = np.array(labels)
+        training_dataset, test_dataset, training_labels, test_labels = train_test_split(dataset, labels, test_size=0.20, random_state=33)
 
         model = self.model()
-        model.fit(training_dataset, training_labels, epochs=2000)
+        model.fit(training_dataset, training_labels, epochs=500)
         test_loss, test_acc = model.evaluate(test_dataset, test_labels)
         print('\nTest accuracy: {}'.format(test_acc))
+        model.save('./../topic_model_new/model.keras')
+      #  tf.keras.models.save_model(model, './../topic_model_new')
+       # tf.saved_model.save(model, './../topic_model_new')
 
     def model(self):
         model = keras.Sequential([
-            keras.layers.Dense(name='layer_1', input_shape=(None, 150), units=150),
-            keras.layers.Dense(name='layer_2', input_shape=(None, 150), units=100),
-            keras.layers.Dense(name='layer_3', input_shape=(None, 100), units=100),
-            keras.layers.Dense(name='layer_4', input_shape=(None, 100), units=100),
-            keras.layers.Dense(name='layer_5', input_shape=(None, 100), units=50),
-            keras.layers.Dense(name='layer_6', input_shape=(None, 50), units=10),
-            keras.layers.Dense(name='layer_7', input_shape=(None, 10), units=10),
+            keras.layers.Dense(150),
+           # keras.layers.Dropout(rate=0.3),
+            keras.layers.Dense(200),
+            keras.layers.Dense(250),
+          #  keras.layers.Dropout(rate=0.3),
+           # keras.layers.Dropout(rate=0.3),
+            keras.layers.Dense(200),
+           # keras.layers.Dropout(rate=0.3),
+            keras.layers.Dense(150),
+           # keras.layers.Dropout(rate=0.3),
+            keras.layers.Dense(100),
+            #keras.layers.Dropout(rate=0.3),
+            keras.layers.Dense(10, activation='softmax'),
         ])
         model.summary()
 
-        model.compile(optimizer='adam',
-                      loss=tf.keras.losses.MeanAbsoluteError,
-                      metrics=[keras.metrics.Accuracy()])
+        initial_learning_rate = 0.001
+        lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+            initial_learning_rate,
+            decay_steps=1000,
+            decay_rate=0.1,
+            staircase=True)
+
+        model.compile(optimizer=tf.keras.optimizers.Adam(),
+                      loss=tf.keras.losses.CategoricalCrossentropy(),
+                      metrics=['accuracy'])
         return model
+
+
+@tf.function
+def custom_metric(y_true, y_pred):
+    acc = tf.math.argmax(y_true, axis=1) == tf.math.argmax(y_pred, axis=1)
+    elements_equal_to_value = tf.equal(acc, True)
+    as_ints = tf.cast(elements_equal_to_value, tf.int32)
+    return tf.reduce_sum(as_ints) / tf.size(acc) * 100
