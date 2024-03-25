aa42d81818250b15175b8cbe85613d5b71511fcf
Kate Makarova <kate.makarova@hotmail.com>
2024-03-24 22:41:26 -0400

    test

--- advertiser/AdvertiserV2.py
+++ advertiser/AdvertiserV2.py
@@ -44,7 +44,7 @@ class AdvertiserV2:
         self.home_base = ''
         self.logged_in = False
         self.custom_l = False
-        self.model = keras.models.load_model(str(settings.BASE_DIR)+'/models/model-2024-03-24.keras')
+        self.model = keras.models.load_model(str(settings.BASE_DIR)+'/topic_model_new.keras')
         self.templates = []
         self.forum_settings = {}
 
