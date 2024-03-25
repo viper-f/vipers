c217c02013e45fcbf56046a18e7c57ad82158aea
Kate Makarova <kate.makarova@hotmail.com>
2024-03-25 19:36:42 -0400

    test

--- models/test.py
+++ models/test.py
@@ -1,3 +1,6 @@
 import tensorflow as tf
+import numpy as np
 
 model = tf.keras.models.load_model('model-2024-03-24.keras')
+prediction = model.predict(np.array([1,0,0,0,0,1,0,0,0,0,1,0,0,0,0]))
+print(prediction)
