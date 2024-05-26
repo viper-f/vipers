0356c5d6902c30205681e3dc3a84ee312ce6fa90
Kate Makarova <kate.makarova@hotmail.com>
2024-05-26 12:20:24 -0400

    remove cors

--- storage/views.py
+++ storage/views.py
@@ -5,18 +5,14 @@ from storage.models import StorageRecord
 
 @csrf_exempt
 def record_put(request):
-    headers = {"Access-Control-Allow-Origin": ''}
-    if request.META['HTTP_REFERER'] in ['http://localhost', 'https://kingscross.f-rpg.me']:
-        headers = {"Access-Control-Allow-Origin": request.META['HTTP_REFERER']}
-
     if request.method == "POST":
         try:
             data = json.loads(request.body)
         except:
-            return JsonResponse({"error": "invalid json"}, headers=headers)
+            return JsonResponse({"error": "invalid json"})
 
         if 'board_id' not in data or 'key' not in data or 'value' not in data:
-            return JsonResponse({"error": "forum, key and value are required values"}, headers=headers)
+            return JsonResponse({"error": "forum, key and value are required values"})
 
         if 'user_id' in data:
             user_id = data.get("user_id")
@@ -38,35 +34,30 @@ def record_put(request):
                 type=type
             )
             record.save()
-            return JsonResponse({"status": "success"}, headers=headers)
+            return JsonResponse({"status": "success"})
 
         # update
         else:
             record = StorageRecord.objects.filter(board_id=data.get("board_id"), user_id=data.get("user_id"), key=data.get("key"))
 
             if record is None:
-                return JsonResponse({"error": "record not found"}, headers=headers)
+                return JsonResponse({"error": "record not found"})
 
             record = StorageRecord(
                 value=data.get('value'),
                 type=type
             )
             record.save()
-            return JsonResponse({"status": "success"}, headers=headers)
+            return JsonResponse({"status": "success"})
 
 def record_get(request):
-    headers = {"Access-Control-Allow-Origin": ''}
-    if request.META['HTTP_REFERER'] in ['http://localhost', 'https://kingscross.f-rpg.me']:
-        headers = {"Access-Control-Allow-Origin": request.META['HTTP_REFERER']}
-
     if request.method == "GET":
 
         data = request.GET
         record = StorageRecord.objects.filter(board_id=data.get("board_id"), user_id=data.get("user_id"),
                                               key=data.get("key")).first()
-
         if record is None:
-            return JsonResponse({"error": "record not found"}, headers=headers)
+            return JsonResponse({"error": "record not found"})
 
         if record.type == "json":
             response = {
@@ -77,5 +68,5 @@ def record_get(request):
                 record.key: record.value
             }
 
-        return JsonResponse(response, headers=headers)
+        return JsonResponse(response)
 
