fbecd2a0092b2d86423c3b46d2379c317aafb5a8
Kate Makarova <kate.makarova@hotmail.com>
2024-05-27 20:09:44 -0400

    new styles

--- dynamicfiles/views.py
+++ dynamicfiles/views.py
@@ -12,7 +12,7 @@ from vipers.settings import BASE_DIR
 def set_cookie(request):
     if request.method == "GET":
         cookie = request.GET.get("cookie")
-        response = HttpResponse()
+        response = HttpResponse("Yes")
 
         max_age = 365 * 24 * 60 * 60
         expires = datetime.datetime.strftime(datetime.datetime.utcnow() + datetime.timedelta(seconds=max_age), "%a, %d-%b-%Y %H:%M:%S GMT")
