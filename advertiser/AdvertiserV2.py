c5d80a1cd1237119e0c102bf357b6aa4cb00ed27
Kate Makarova <kate.makarova@hotmail.com>
2024-09-28 13:39:50 -0400

    more flags

--- advertiser/AdvertiserV2.py
+++ advertiser/AdvertiserV2.py
@@ -35,6 +35,12 @@ class AdvertiserV2:
         options.add_argument('--headless')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-gpu')
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-dev-tools")
+        options.add_argument("--no-zygote")
+        options.add_argument("--single-process")
+        options.add_argument("--remote-debugging-pipe")
+        options.add_argument("--log-path=/tmp")
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
         prefs = {"profile.managed_default_content_settings.images": 2}
         options.add_experimental_option("prefs", prefs)
